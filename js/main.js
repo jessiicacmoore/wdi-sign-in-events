05c9ac5a2db2ca0067e96558bb87712fbdd5c07d
Jessica Moore <jessica.cmoore1@gmail.com>
2019-06-18 13:44:41 -0400

    Close modal when close btn is clicked

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', function(){
   signInBtn = document.querySelector('.signin');
+  closeBtn = document.querySelector('.close');
   modal = document.querySelector('.modal');
 
+  // When the user presses the .signin button, display the modal window
   signInBtn.addEventListener('click', () => {
     modal.style.display = 'block';
   })
+
+  // When the user presses the .close button, hide the modal window
+  closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+  })
+
+
 });
