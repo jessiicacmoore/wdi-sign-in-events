373e588d29bc355c5624656b65526ed16eb03fad
Jessica Moore <jessica.cmoore1@gmail.com>
2019-06-18 13:56:28 -0400

    When submit btn is clicked add class error to inputs

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 document.addEventListener('DOMContentLoaded', function(){
-  signInBtn = document.querySelector('.signin');
-  closeBtn = document.querySelector('.close');
-  modal = document.querySelector('.modal');
+  const signInBtn = document.querySelector('.signin');
+  const closeBtn = document.querySelector('.close');
+  const submitBtn = document.querySelector('.submit');
+  const modal = document.querySelector('.modal');
+  const inputs = document.querySelectorAll('input');
 
   // When the user presses the .signin button, display the modal window
   signInBtn.addEventListener('click', () => {
     modal.style.display = 'block';
-  })
+  });
 
   // When the user presses the .close button, hide the modal window
   closeBtn.addEventListener('click', () => {
     modal.style.display = 'none';
-  })
+  });
 
-
+  // When the user presses the .submit button, add an .error class to both input elements
+  submitBtn.addEventListener('click', () => {
+    inputs.forEach(input => {
+      input.classList.add('error');
+    })
+  });
 });
